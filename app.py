de681f482df3f13a14a9634f261c74e3de6380c4
Mariyam Siddiqui <153370326+MariyamSiddiqui@users.noreply.github.com>
2024-07-14 20:08:06 +0000

    added special text

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+BEDDESIGNS = [
+  {
+    'Serial No': 1,
+    'Bed': 'Double Bed',
+    'Wood':'Trek',
+    'Color': 'Light Blue',
+    'Price': '£150'
+  },
+  {
+    'Serial No': 2,
+    'Bed': 'Double Bed',
+    'Wood':'Bom',
+    'Color': 'Orange',
+    'Price': '£250'
+  },
+  {
+    'Serial No': 3,
+    'Bed': 'Single Bed',
+    'Wood':'Trek',
+    'Color': 'Light Purple',
+    'Price': '£50'
+  }
+  
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', beddesigns=BEDDESIGNS )
 
 
 if __name__ == "__main__":
